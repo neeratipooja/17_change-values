ba311395abf13f07c705827ed7f8586e25da0293
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-05 17:54:48 +0530

    Add files via upload

---
+++ changevalues.py
@@ -0,0 +1,13 @@
+"""
+DAY:friday
+DATE:5th nov 2022
+AUTHOR:pooja
+"""
+a=5; b=3.4;c='py';d='on'; e=8; f=8.4;g='58'
+print(a+e) #int+int -int #13
+print(a+e,type(a+e)) #13int
+print(a+b,type(a+b))#int+float=float
+                     #8.4 float
+#print(a+c,type(a+c))                  
+print(a+f,type(a+f))                     
+                   
